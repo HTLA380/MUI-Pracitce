10da8ff85c2f3d554fe258e8ce05cf3805c2bf9c
Htet Lin <htetlin099798@gmail.com>
2023-12-24 10:12:56 +0630

    Adding Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
-import { Container, Typography } from "@mui/material";
+import { Box, Stack } from "@mui/material";
+import Sidebar from "./components/Sidebar";
+import Rightbar from "./components/Rightbar";
+import Feed from "./components/Feed";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <>
-      <Container>
-        <Typography variant="h1" color={"primary.light"}>
-          Typo
-        </Typography>
-      </Container>
-    </>
+    <Box>
+      <Navbar />
+      <Stack spacing={2} direction="row" justifyContent="space-between">
+        <Sidebar />
+        <Feed />
+        <Rightbar />
+      </Stack>
+    </Box>
   );
 }
 
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,104 @@
+import { Mail, Notifications, Pets } from "@mui/icons-material";
+import {
+  AppBar,
+  Avatar,
+  Badge,
+  Box,
+  InputBase,
+  Menu,
+  MenuItem,
+  Toolbar,
+  Typography,
+  styled,
+} from "@mui/material";
+import React, { useState } from "react";
+import profileImg from "../assets/htet-aung-lin.png";
+
+const StyledToolbar = styled(Toolbar)({
+  display: "flex",
+  justifyContent: "space-between",
+});
+
+const Search = styled("div")(({ theme }) => ({
+  backgroundColor: "white",
+  padding: "0 10px",
+  borderRadius: theme.shape.borderRadius,
+  width: "40%",
+}));
+
+const Icons = styled(Box)(({ theme }) => ({
+  display: "none",
+  alignItems: "center",
+  gap: "1rem",
+  [theme.breakpoints.up("sm")]: { display: "flex" },
+}));
+
+const UserBox = styled(Box)(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  gap: "0.4rem",
+  [theme.breakpoints.up("sm")]: { display: "none" },
+}));
+
+const Navbar: React.FC = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  return (
+    <AppBar position="sticky">
+      <StyledToolbar>
+        <Typography variant="h6" sx={{ display: { xs: "none", sm: "block" } }}>
+          HTLA
+        </Typography>
+        <Pets sx={{ display: { xs: "block", sm: "none" } }} />
+        <Search>
+          <InputBase placeholder="search..." />
+        </Search>
+        <Icons>
+          <Badge badgeContent={4} color="error">
+            <Mail />
+          </Badge>
+          <Badge badgeContent={2} color="error">
+            <Notifications />
+          </Badge>
+          <Avatar
+            sx={{ width: 30, height: 30 }}
+            alt="Htet Aung Lin"
+            src={profileImg}
+            onClick={() => setIsOpen(true)}
+          />
+        </Icons>
+        <UserBox>
+          <Avatar
+            sx={{ width: 30, height: 30 }}
+            alt="Htet Aung Lin"
+            src={profileImg}
+            onClick={() => setIsOpen(true)}
+          />
+          <Typography variant="body2" sx={{ fontWeight: "semibold" }}>
+            HTLA
+          </Typography>
+        </UserBox>
+      </StyledToolbar>
+
+      <Menu
+        id="menu"
+        aria-labelledby="button"
+        open={isOpen}
+        onClose={() => setIsOpen(false)}
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "right",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}>
+        <MenuItem>Profile</MenuItem>
+        <MenuItem>My account</MenuItem>
+        <MenuItem>Logout</MenuItem>
+      </Menu>
+    </AppBar>
+  );
+};
+
+export default Navbar;
