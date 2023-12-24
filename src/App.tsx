fc56a1231a88a9bd662d8d63727d2d853a49931f
Htet Lin <htetlin099798@gmail.com>
2023-12-24 06:42:02 +0630

    removing unnecessary files

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import "./App.css";
 import { Container, Typography } from "@mui/material";
 
 function App() {
   return (
     <>
-      <Container sx={{ bgcolor: "red", height: "100vh", width: "100vw" }}>
-        <Typography variant="h1">Typo</Typography>
+      <Container>
+        <Typography variant="h1" color={"primary.light"}>
+          Typo
+        </Typography>
       </Container>
     </>
   );
