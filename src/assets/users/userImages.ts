c03d41da90324614c3887e794e5b91bc38f4ddb9
Htet Lin <htetlin099798@gmail.com>
2023-12-25 10:40:31 +0630

    right bar

---
+++ src/assets/users/userImages.ts
@@ -0,0 +1,7 @@
+import profile1Img from "./profile1.png";
+import profile2Img from "./profile2.jpg";
+import profile3Img from "./profile3.jpg";
+import profile4Img from "./profile4.jpg";
+import profile5Img from "./profile5.jpg";
+
+export { profile1Img, profile2Img, profile3Img, profile4Img, profile5Img };
--- src/components/Rightbar.tsx
+++ src/components/Rightbar.tsx
@@ -1,12 +1,162 @@
-import { Box } from "@mui/material";
+import {
+  Avatar,
+  AvatarGroup,
+  Box,
+  Divider,
+  ImageList,
+  ImageListItem,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  Typography,
+} from "@mui/material";
 import React from "react";
+import {
+  profile2Img,
+  profile3Img,
+  profile4Img,
+  profile5Img,
+} from "../assets/users/userImages";
 
 const Rightbar: React.FC = () => {
   return (
     <Box flex={1} sx={{ display: { xs: "none", sm: "block" } }}>
-      rightbar
+      <Box position="fixed" top={0} right={0} width={300} p={"5rem 2rem"}>
+        <Typography variant="h6" fontWeight="normal" mt={3}>
+          Online Friends
+        </Typography>
+        <AvatarGroup
+          max={4}
+          sx={{
+            justifyContent: "start",
+            mt: "0.25rem",
+            pb: "1rem",
+            borderBottom: "1px solid #d4d4d4",
+          }}>
+          <Avatar alt="Remy Sharp" src={profile5Img} />
+          <Avatar alt="Travis Howard" src={profile2Img} />
+          <Avatar alt="Agnes Walker" src={profile3Img} />
+          <Avatar alt="Trevor Henderson" src={profile4Img} />
+        </AvatarGroup>
+        <Typography variant="h6" fontWeight="normal" mt={2}>
+          Latest Photos
+        </Typography>
+        <ImageList
+          sx={{ width: "100%", pb: "2rem", borderBottom: "1px solid #d4d4d4" }}
+          cols={3}
+          rowHeight={100}>
+          {itemData.map((item) => (
+            <ImageListItem key={item.img}>
+              <img
+                srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+                src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
+                alt={item.title}
+                loading="lazy"
+              />
+            </ImageListItem>
+          ))}
+        </ImageList>
+
+        <List
+          sx={{
+            width: "100%",
+            maxWidth: 360,
+            bgcolor: "background.paper",
+          }}>
+          <ListItem alignItems="flex-start">
+            <ListItemAvatar>
+              <Avatar alt="Remy Sharp" src={profile3Img} />
+            </ListItemAvatar>
+            <ListItemText
+              primary="Brunch this weekend?"
+              secondary={
+                <React.Fragment>
+                  <Typography
+                    sx={{ display: "inline" }}
+                    component="span"
+                    variant="body2"
+                    color="text.primary">
+                    Ali Connors
+                  </Typography>
+                  {" — I'll be in your neighborhood doing errands this…"}
+                </React.Fragment>
+              }
+            />
+          </ListItem>
+          <Divider variant="inset" component="li" />
+          <ListItem alignItems="flex-start">
+            <ListItemAvatar>
+              <Avatar alt="Travis Howard" src={profile2Img} />
+            </ListItemAvatar>
+            <ListItemText
+              primary="Summer BBQ"
+              secondary={
+                <React.Fragment>
+                  <Typography
+                    sx={{ display: "inline" }}
+                    component="span"
+                    variant="body2"
+                    color="text.primary">
+                    to Scott, Alex, Jennifer
+                  </Typography>
+                  {" — Wish I could come, but I'm out of town this…"}
+                </React.Fragment>
+              }
+            />
+          </ListItem>
+          <Divider variant="inset" component="li" />
+          <ListItem alignItems="flex-start">
+            <ListItemAvatar>
+              <Avatar alt="Cindy Baker" src={profile5Img} />
+            </ListItemAvatar>
+            <ListItemText
+              primary="Oui Oui"
+              secondary={
+                <React.Fragment>
+                  <Typography
+                    sx={{ display: "inline" }}
+                    component="span"
+                    variant="body2"
+                    color="text.primary">
+                    Sandra Adams
+                  </Typography>
+                  {" — Do you have Paris recommendations? Have you ever…"}
+                </React.Fragment>
+              }
+            />
+          </ListItem>
+        </List>
+      </Box>
     </Box>
   );
 };
+
+const itemData = [
+  {
+    img: "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e",
+    title: "Breakfast",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1551782450-a2132b4ba21d",
+    title: "Burger",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1522770179533-24471fcdba45",
+    title: "Camera",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c",
+    title: "Coffee",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1533827432537-70133748f5c8",
+    title: "Hats",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1558642452-9d2a7deb7f62",
+    title: "Honey",
+  },
+];
 
 export default Rightbar;
