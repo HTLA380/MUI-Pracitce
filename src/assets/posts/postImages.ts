3aedd341218acb437fcf91d074ea66662402b15a
Htet Lin <htetlin099798@gmail.com>
2023-12-25 10:40:19 +0630

    adding profile image

---
+++ src/assets/posts/postImages.ts
@@ -0,0 +1,6 @@
+import post1Img from "./post1.jpg";
+import post2Img from "./post2.jpg";
+import post3Img from "./post3.jpg";
+import post4Img from "./post4.jpg";
+
+export { post1Img, post2Img, post3Img, post4Img };
